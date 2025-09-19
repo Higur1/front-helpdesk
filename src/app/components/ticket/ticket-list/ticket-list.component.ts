9cd0c3e9797dea9d6bb9232442946be8b52bceb1
Higur1 <Higor.hungria466@gmail.com>
2025-09-19 17:57:31 -0300

    CREATE - filter and findAll method to ticket

--- src/app/components/ticket/ticket-list/ticket-list.component.ts
+++ src/app/components/ticket/ticket-list/ticket-list.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
-import { Customer } from 'src/app/models/customer';
 import { Ticket } from 'src/app/models/ticket';
+import { TicketService } from 'src/app/services/ticket.service';
 
 @Component({
   selector: 'app-ticket-list',
@@ -10,35 +10,54 @@ import { Ticket } from 'src/app/models/ticket';
   styleUrls: ['./ticket-list.component.css']
 })
 export class TicketListComponent implements OnInit {
-  ELEMENT_DATA: Ticket[] = [
-    {
-      id: 1,
-      title: 'Ticket 1',
-      createdAt: '17/09/2026',
-      closedAt: '17/09/2026',
-      priority: 'MEDIUM',
-      status: 'OPEN',
-      observation: 'First ticket',
-      technicianId: 1,
-      technicianName: 'Higor',
-      customerId: 13,
-      customerName: 'Linus Torvalds'
-    }
-  ] 
+  ELEMENT_DATA: Ticket[] = [];
+  FILTERED_DATA: Ticket[] = [];
 
-  displayedColumns: string[] = ['id', 'title', 'createdAt', 'closedAt', 'customerName', 'technicianName', 'priority', 'status', 'actions'];
+  displayedColumns: string[] = [
+    'id',
+    'title',
+    'createdAt',
+    'closedAt',
+    'customerName',
+    'technicianName',
+    'priority',
+    'status',
+    'actions'
+  ];
+
   dataSource = new MatTableDataSource<Ticket>(this.ELEMENT_DATA);
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  constructor() { }
+  constructor(private service: TicketService) { }
 
   ngOnInit(): void {
+    this.findAll();
+
   }
 
-  applyFilter(event: Event){
+  applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
+  findAll(): void {
+    this.service.findAll().subscribe(response => {
+      this.ELEMENT_DATA = response;
+      this.dataSource = new MatTableDataSource<Ticket>(response);
+      this.dataSource.paginator = this.paginator;
+    });
+  }
+
+  orderByStatus(status: any): void {
+    let list: Ticket[] = [];
+    this.ELEMENT_DATA.forEach(element => {
+      if (element.status == status) {
+        list.push(element);
+      }
+    });
+    this.FILTERED_DATA = list;
+    this.dataSource = new MatTableDataSource<Ticket>(list);
+    this.dataSource.paginator = this.paginator;
+  }
 }
---
+++ src/app/services/ticket.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Ticket } from '../models/ticket';
+import { HttpClient } from '@angular/common/http';
+import { API_CONFIG } from '../config/api.config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TicketService {
+
+  constructor(private http:HttpClient) { }
+
+  findAll(): Observable<Ticket[]>{
+    return this.http.get<Ticket[]>(`${API_CONFIG.baseUrl}/tickets`)
+  }
+}
