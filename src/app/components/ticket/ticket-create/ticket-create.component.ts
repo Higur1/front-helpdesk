d861cf35c3f36629167f420d4f0a78e7272ff31e
Higur1 <Higor.hungria466@gmail.com>
2025-09-19 21:24:54 -0300

    Validation - form validation for the ticket-create component

--- src/app/components/ticket/ticket-create/ticket-create.component.ts
+++ src/app/components/ticket/ticket-create/ticket-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-ticket-create',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TicketCreateComponent implements OnInit {
 
+  priority: FormControl = new FormControl(null, [Validators.required])
+  status: FormControl = new FormControl(null, [Validators.required])
+  title: FormControl = new FormControl(null, [Validators.required])
+  observaton: FormControl = new FormControl(null, [Validators.required])
+  technician: FormControl = new FormControl(null, [Validators.required])
+  customer: FormControl = new FormControl(null, [Validators.required])
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  fieldValidation(): boolean {
+    return this.priority.valid &&
+      this.status.valid &&
+      this.title.valid &&
+      this.observaton.valid &&
+      this.technician.valid &&
+      this.customer.valid 
+  }
 }
