322d2c6f0fed0675abfcb5dece9e28a2688c75d5
Higur1 <Higor.hungria466@gmail.com>
2025-09-22 23:18:49 -0300

    FINISHED - ticket-read component

--- src/app/components/ticket/ticket-read/ticket-read.component.ts
+++ src/app/components/ticket/ticket-read/ticket-read.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Ticket } from 'src/app/models/ticket';
+import { TicketService } from 'src/app/services/ticket.service';
 
 @Component({
   selector: 'app-ticket-read',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TicketReadComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ticket: Ticket = {
+    priority: '',
+    status: '',
+    title: '',
+    observation: '',
+    customerId: '',
+    customerName: '',
+    technicianId: '',
+    technicianName: ''
   }
 
+  constructor(
+    private ticketService: TicketService,
+    private toastrService: ToastrService,
+    private route: ActivatedRoute,
+  ) { }
+
+  ngOnInit(): void {
+    this.ticket.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
+  }
+
+  findById(): void {
+    this.ticketService.findById(this.ticket.id).subscribe(response => {
+      console.log(response)
+      this.ticket = response;
+    }, ex => {
+      this.toastrService.error(ex.error.error, 'Error');
+    })
+  }
 }
